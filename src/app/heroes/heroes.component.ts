4ff7d7db1601d2fc7412b93fdef9f0b711d7b81f
AzureAD\IngoTaag <ingo.taag@profect.de>
2023-10-18 09:58:50 +0200

    Dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService} from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
+
 export class HeroesComponent {
-  constructor(private heroService: HeroService) {}
+  
+  constructor(private heroService: HeroService,private messageService:MessageService) {}
+  
+  //Properties
   heroes:Hero[]=[];
   selectedHero?: Hero;
+  
+  //Methods
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('Heroes-Componment: Selected heor id=$(hero.id)');
   }
 
-  getHeroes() {
-    this.heroService.getHeroes().subscribe(heroes=>this.heroes=this.heroes);
+  getHeroes() :void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
 
   }
 
